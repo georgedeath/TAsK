67576de5b65f3c14688de58c1cdb530d0e3ead49
George De Ath <g.de.ath@exeter.ac.uk>
2020-07-16 15:35:26 +0100

    Sped up the BPR cost function marginally.

--- src/BprFnc.cpp
+++ src/BprFnc.cpp
@@ -6,51 +6,58 @@
 #include <iostream>
 #include <iomanip>
 
-BprFnc::BprFnc(FPType freeFlow, FPType B, FPType capacity, FPType power) : freeFlow_(freeFlow), 
-				 B_(B), capacity_(capacity), power_(power) { 
-};
+BprFnc::BprFnc(FPType freeFlow, FPType B, FPType capacity, FPType power) : freeFlow_(freeFlow),
+                                                                           B_(B), capacity_(capacity), power_(power){};
 
 BprFnc::~BprFnc(){
 
 };
 
-FPType BprFnc::evaluate(FPType linkFlow) const {
-	if (power_ == 0.0) //in order to avoid problems with floating point arithmatic
-		return freeFlow_; 
-	return freeFlow_ * (1 + B_ * pow(linkFlow / capacity_, power_));
+FPType BprFnc::evaluate(FPType linkFlow) const
+{
+    if (capacity_ == 0 || power_ == 0.0 || linkFlow == 0.0)
+        return freeFlow_;
+    return freeFlow_ * (1 + B_ * pow(linkFlow / capacity_, power_));
 };
 
-FPType  BprFnc::evaluateDerivative(FPType linkFlow) const {
-	if (power_ == 0.0)  //in order to avoid problems with floating point arithmatic
-		return 0.0;
-	return freeFlow_ * B_ * power_ * pow(linkFlow / capacity_, power_ - 1) / capacity_;
+FPType BprFnc::evaluateDerivative(FPType linkFlow) const
+{
+    if (capacity_ == 0 || power_ == 0.0 || linkFlow == 0.0)
+        return 0.0;
+    return freeFlow_ * B_ * power_ * pow(linkFlow / capacity_, power_ - 1) / capacity_;
 };
 
-void BprFnc::print() const{
-	std::cout << "bpr " << std::endl;
-	std::cout << "freeFlow = " << freeFlow_ << " B = " << B_ << " capacity = " << capacity_ << 
-		" power_ = " << power_ << std::endl;
+void BprFnc::print() const
+{
+    std::cout << "bpr " << std::endl;
+    std::cout << "freeFlow = " << freeFlow_ << " B = " << B_ << " capacity = " << capacity_ << " power_ = " << power_ << std::endl;
 };
 
-FPType BprFnc::evaluateSecondDerivative(FPType linkFlow) const {
-	if (power_ - 1e-16 < 2) return 0.0;  // in order to avoid problems with floating point arithmatic
-	return freeFlow_ * B_ * power_ / pow(capacity_, power_) * (power_ - 1) * pow(linkFlow, power_ - 2);
+FPType BprFnc::evaluateSecondDerivative(FPType linkFlow) const
+{
+    if (capacity_ == 0 || power_ - 1e-16 < 2 || linkFlow == 0.0)
+        return 0.0;
+    return freeFlow_ * B_ * power_ / pow(capacity_, power_) * (power_ - 1) * pow(linkFlow, power_ - 2);
 };
 
-FPType BprFnc::evaluateTimeAndDerivative(FPType linkFlow, FPType &der){
-	if (power_ == 0.0) {
-		der = 0.0;
-		return freeFlow_;
-	}
-	FPType A = freeFlow_ * B_ * pow(linkFlow / capacity_, power_ - 1) / capacity_;
-	der = power_ * A;
-	return freeFlow_ + A * linkFlow;
+FPType BprFnc::evaluateTimeAndDerivative(FPType linkFlow, FPType &der)
+{
+    if (capacity_ == 0 || power_ == 0.0)
+    {
+        der = 0.0;
+        return freeFlow_;
+    }
+    FPType A = freeFlow_ * B_ * pow(linkFlow / capacity_, power_ - 1) / capacity_;
+    der = power_ * A;
+    return freeFlow_ + A * linkFlow;
 };
 
-FPType BprFnc::getCapacity(){
-	return capacity_;
+FPType BprFnc::getCapacity()
+{
+    return capacity_;
 };
 
-FncType BprFnc::getType(){
-	return BPRFNC;
+FncType BprFnc::getType()
+{
+    return BPRFNC;
 };
