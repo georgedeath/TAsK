78ec77255ff12929532d1436aa68882246e6d666
George De Ath <g.de.ath@exeter.ac.uk>
2020-07-18 07:04:40 +0100

    Saturn cost function. The second derivative needs to be set to the first derivate; this is obviously wrong, but works.

--- src/SatFnc.cpp
+++ src/SatFnc.cpp
@@ -48,13 +48,27 @@ void SatFnc::print() const
 FPType SatFnc::evaluateSecondDerivative(FPType linkFlow) const
 {
     // not used anywhere
-    return 0.0;
+    return 1800.0 / capacity_;
 };
 
 FPType SatFnc::evaluateTimeAndDerivative(FPType linkFlow, FPType &der)
 {
-    der = evaluateDerivative(linkFlow);
-    return evaluate(linkFlow);
+    if (capacity_ == 0 || power_ - 1e-16 < 2 || linkFlow == 0.0)
+    {
+        der = 0.0;
+        return freeFlow_;
+    }
+    if (linkFlow <= capacity_)
+    {
+        // bpr first derivative
+        der = freeFlow_ * B_ * power_ * pow(linkFlow / capacity_, power_ - 1) / capacity_;
+        return freeFlow_ * (1 + B_ * pow(linkFlow / capacity_, power_));
+    }
+    // else the saturn deriv: m / (2 * capacity dispersion)
+    der = 1800.0 / capacity_;
+    
+    // 0.5 * 3600 = 1800  -- 3600 is the modelperiod for everything
+    return freeFlow_ * (1 + B_) + 1800.0 * (linkFlow - capacity_) / capacity_;
 };
 
 FPType SatFnc::getCapacity()
